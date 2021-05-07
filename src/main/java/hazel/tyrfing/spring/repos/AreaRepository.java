7ba74b56d84fdb6d351051beb69d621ad03830c3
hazel <liam.k.becker@gmail.com>
2021-05-07 14:47:14 -0400

    Separated react

--- src/main/java/hazel/tyrfing/spring/repos/AreaRepository.java
+++
@@ -1,9 +0,0 @@
-package hazel.tyrfing.spring.repos;
-
-import hazel.tyrfing.models.utility.Area;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AreaRepository extends CrudRepository<Area, Long> {
-}
